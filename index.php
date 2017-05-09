0d3c92020e8e953a5820946a99d9fb0149760091
Samartha K V <samarthakv29@gmail.com>
2017-05-08 21:05:26 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>Zulu Moving Company</title>
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" media="all" rel="stylesheet" type="text/css">
+    <meta content="Website for Zulu Moving" name="description">
+    <meta content="RightApps" name="author"><!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"><!-- Custom CSS -->
+    <link href="css/style1.css" rel="stylesheet">
+    <link href="css/reset.css" rel="stylesheet"><!-- Custom fontss -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" media="all" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kelly+Slab' rel='stylesheet' type='text/css'><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesnt work if you view the page via file:// !-->
+
+    <script language="javascript" type="text/javascript">
+    </script>
+</head>
+
+<body>
+    <script type="text/javascript">
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            
+            ga('create', 'UA-81897421-1', 'auto');
+            ga('send', 'pageview');
+            
+            
+    </script>
+
+    <nav class="navbar navbar-default navbar-fixed-top" style="margin-bottom: 100px;">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+
+
+            <div class="navbar-header page-scroll">
+                <button class="navbar-toggle" data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" style="background-color: black;" type="button"><span class="sr-only">Nav</span>
+                <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button> <a class="navbar-brand" href="#page-top">
+                <div class="img-responsive logo" onmouseout="$(this).removeClass('logoShine');" onmouseover="$(this).addClass('logoShine');">
+                </div>
+
+
+                <div class="logotxt">
+                    Zulu Moving Company
+                </div></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                    </li>
+
+
+                    <li class="page-scroll">
+                        <a href="#aboutus">About Us</a>
+                    </li>
+
+
+                    <li class="page-scroll">
+                        <a href="#contactus">Contact</a>
+                    </li>
+
+
+                    <li class="page-scroll">
+                        <a class="phoneno" href="tel:1-978-847-8352">Phone:978-847-8352</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+
+    <div class="carousel slide" data-ride="carousel" id="myCarousel">
+        <!-- Wrapper for slides -->
+
+
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img alt="Movers" class="c3" src="img/van.jpg">
+
+                <div class="carousel-caption">
+                    <h3 class="heading">We are there for you, everywhere!</h3>
+
+
+                    <p class="text">Across cities, fast, reliable and efficient! Request Quote now!</p>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img alt="Package" class="c4" src="img/rsz_packing.jpg">
+
+                <div class="carousel-caption">
+                    <h3 class="heading">Handling and Packaging</h3>
+
+
+                    <p class="text">We secure your package, we bring the packaging materials too. Ease moving!</p>
+                </div>
+            </div>
+            <!-- Left and right controls -->
+            <a class="left carousel-control" data-slide="prev" href="#myCarousel" role="button"><span aria-hidden="true" class="glyphicon glyphicon-chevron-left"></span> <span class=
+            "sr-only">Previous</span></a> <a class="right carousel-control" data-slide="next" href="#myCarousel" role="button"><span aria-hidden="true" class=
+            "glyphicon glyphicon-chevron-right"></span> <span class="sr-only">Next</span></a>
+        </div>
+    </div>
+
+
+    <div class="row" id="aboutus" style="border-bottom: thick solid chocolate; border-top: thin solid chocolate; padding: 5%;">
+        <div class="text-center headings" style="font-family: &quot;Kelly Slab&quot;">
+            What We Do
+        </div>
+
+
+        <div class="row text-justify" style="font-family: &quot;Kelly Slab&quot;">
+            <div class="col-xs-6" style="font-family: &quot;Kelly Slab&quot;">
+                <div class="text-center sub-headings" style="padding: 2%">
+                    Specialities
+                </div>
+
+
+                <p>Here at Zulu Moving Company we are dedicated in giving the best customer service possible. We treat your belonging as we would with our own. We are fully insured covering all
+                possible damages, we understand somethings are invaluable we do our best to prevent damages from happening. we are experts at local, long distance, residential, office and piano
+                moving.</p>
+
+
+                <p>We are currently accepting local jobs in Massachusetts, Southern NH and all surrounding areas. We are known for our outstanding dedication and customer service competence. We are
+                also accepting long distance jobs.</p>
+            </div>
+
+
+            <div class="col-xs-6">
+                <div class="text-center sub-headings" style="padding: 2%">
+                    About
+                </div>
+
+
+                <p>Established in 2012. Zulu Moving Company offers top notch moving services. We are based out of Lowell, MA and have been in the moving business since 2012. We are licensed and
+                Insured. Please feel free to contact us today we are your last minute moving experts. guaranteed same day service. give us a call and one of our representatives will gladly assist
+                you</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row" id="contactus" style="padding:10px;padding-right:7%;padding-left:7%;border-bottom: thick solid brown; border-top: thin solid brown;">
+        <div class="row">
+            <div class="text-center headings" style="font-family: &quot;Kelly Slab&quot;">
+                Request a Quote
+            </div>
+
+
+            <div class="col-xs-6 text-justify" style="font-family: &quot;Kelly Slab&quot;">
+                <form id="requestor" method="post" name="requestor" onsubmit="return validateForm(); sbmtScc();" style="font-family: &quot;Kelly Slab&quot;">
+                    <table class="table" id="ContactTable">
+                        <tr>
+                            <td>Name:</td>
+
+                            <td><input name="username" placeholder="Enter your name" type="text">
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td>Email:</td>
+
+                            <td><input name="email" placeholder="Enter your Email ID" type="text">
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td>Phone:</td>
+
+                            <td><input name="phone" placeholder="Enter your Phone No" type="text">
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td>Starting location:</td>
+
+                            <td><input name="from" placeholder="Enter source location" type="text">
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td>Destination:</td>
+
+                            <td><input name="to" placeholder="Enter destination" type="text">
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td>Details:</td>
+
+                            <td>
+                            <textarea cols="20" placeholder="Enter your query. Please be sure to include all the details. Example: 2 stairways/ long passage/ long hallway." rows="4"></textarea>
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td>
+                            </td>
+
+                            <td><button id="btnSub" type="submit"><span>Submit</span></button>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+
+
+            <div class="col-xs-6" style="display:block; font-family: &quot;Kelly Slab&quot;">
+                <table id="tblPrices" style="border:none">
+                    <tr>
+                        <td>2 Movers + Truck -</td>
+
+                        <td><strong>$95/hr</strong>
+                        </td>
+                    </tr>
+
+
+                    <tr>
+                        <td>3 Movers + Truck -</td>
+
+                        <td><strong>$115/hr</strong>
+                        </td>
+                    </tr>
+
+
+                    <tr>
+                        <td>4 Movers + Truck -</td>
+
+                        <td><strong>$125/hr</strong>
+                        </td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Flat Rate</td>
+
+                        <td><strong>$800</strong>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container" id="reviews" >
+        <h3 class="row text-center headings">Our Reviews</h3>
+        <div class=" col-xs-6"><div class="embed-responsive embed-responsive-16by9 row">
+        	<iframe class="embed-responsive-item" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fashley.smallwood.31%2Fposts%2F10153990986414378%3A0&amp;" width="" height=""
+          class="c2" scrolling="no" frameborder="0" allowtransparency="true"></iframe>
+        
+        
+        </div><div class="embed-responsive embed-responsive-16by9 row">
+        	<iframe class="embed-responsive-item" class=" "src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fvant.kham%2Fposts%2F1071502119554880%3A1&amp;" width="" height="" class="c2"
+          scrolling="no" frameborder="0" allowtransparency="true"></iframe>
+        
+        </div></div>
+        <div class="col-xs-6"><a href="http://bit.ly/2btuHca"><img class="img-responsive" src="img/reviews gogle.PNG"></a></div>
+        </div>
+
+
+    <div class="container text-center footer" style="font-family:'Kelly Slab'; font-weight:bold;">
+        <div class="footer-above container">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-xs-6">
+                        <ul class="lightTxt sub-headings">
+                            <li>
+                                <p>Hassan Bukenya</p>
+                            </li>
+
+
+                            <li>
+                                <p>Zulu Moving Comany</p>
+                            </li>
+
+
+                            <li>
+                                <p>117 Durrent St</p>
+                            </li>
+
+
+                            <li>
+                                <p>Lowell, MA 01854</p>
+
+
+                                <p>
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div class="footer-col col-xs-6">
+                        <h3 class="lightTxt">Check us out</h3>
+                        <br>
+
+
+                        <ul class="list-inline">
+                            <li>
+                                <a class="btn-social btn-outline" href="https://www.facebook.com/zulumoving"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+
+
+                            <li>
+                                <a class="btn-social btn-outline" href="https://plus.google.com/100400948126936096675"><i class="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <strong style="font-family:'Kelly Slab'; font-weight:bold;">zulumoving.com, 2010 Copyright &copy;
+                    </strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/classie.js">
+    </script> 
+    <script src="js/cbpAnimatedHeader.js">
+    </script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js" type="text/javascript">
+    </script> 
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript">
+    </script> 
+    <script src="js/tools.js">
+    </script>
+</body>
+</html>
---
+++ js/tools.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+	$("#btnSub").find("span").mouseover(function (){
+		$(this).addClass('anim');
+	});	
+	
+	
+	
+	
+});
+
+function sbmtScc(){
+	
+}
+
+function validateForm(){
+				
+				var x = document.forms["requestor"]["username"].value;
+				if (x == null || x == "") {
+					alert("Username must be filled out");
+					return false;
+				}
+				
+				var x = document.forms["requestor"]["email"].value;
+				if (x == null || x == "") {
+					alert("Email must be filled out");
+					return false;
+				}
+				
+                var x = document.forms["requestor"]["phone"].value;
+				if (x == null || x == "") {
+					alert("Phone must be filled out");
+					return false;
+					
+					var x = document.forms["requestor"]["from"].value;
+					if (x == null || x == "") {
+						alert("Package source location must be filled out");
+						return false;
+					}
+					
+					var x = document.forms["requestor"]["to"].value;
+					if (x == null || x == "") {
+						alert("Package destination location must be filled out");
+						return false;
+					}
+					return false;
+				}
+			}
